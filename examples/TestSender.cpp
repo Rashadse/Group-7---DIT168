3a5caa92d521040c6581290d381bcf03a9ee7148
maansthoernvik <thornvik.mans@gmail.com>
2018-02-18 13:45:37 +0100

    Added the two test programs, one sender and one receiver. Testing not done

---
+++ examples/TestSender.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN
+
+#include "libs/catch.hpp"
+
+
+// Verification test case to see catch is working.
+TEST_CASE("Arbitrary test 1.") {
+    REQUIRE(true);
+}
---
+++ examples/receiver.cpp
@@ -0,0 +1,19 @@
+#include <chrono>
+#include <iostream>
+
+#include "cluon/UDPReceiver.hpp"
+
+int main(int /*argc*/, char** /*argv*/) {
+    cluon::UDPReceiver receiver("0.0.0.0", 1235, []
+        (std::string &&data, std::string &&/*from*/, std::chrono::system_clock::time_point &&/*timepoint*/) noexcept {
+            std::cout << "Received " << data.size() << " bytes!" << std::endl;
+        }
+    );
+    
+    using namespace std::literals::chrono_literals;
+    while (receiver.isRunning()) {
+        std::this_thread::sleep_for(1s);
+    }
+    
+    return 0;
+}

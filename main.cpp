ab708ae55dbec5b09cb3a5a0c7e3260c1805eb44
maansthoernvik <thornvik.mans@gmail.com>
2018-03-18 13:25:19 +0100

    Initial test setup for Car->car comms

--- main.cpp
+++ main.cpp
@@ -3,8 +3,31 @@
 #include "v2v.hpp"
 
 
+using namespace std;
 int main(int /*argc*/, char** /*argv*/) {
-    std::cout << "Ran" << std::endl;
+    shared_ptr<V2VService> v2vService = make_shared<V2VService>();
 
-    return 0;
+    while (true) {
+        int choice;
+        cout << "Which message would you like to send?" << std::endl;
+        cout << "(1) AnnouncePresence" << std::endl;
+        cout << "(2) FollowRequest" << std::endl;
+        cout << "(3) FollowResponse" << std::endl;
+        cout << "(4) StopFollow" << std::endl;
+        cout << "(5) LeaderStatus" << std::endl;
+        cout << "(6) FollowerStatus" << std::endl;
+        cout << "(#) Nothing, just quit." << std::endl;
+        cout << ">> ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1: v2vService->announcePresence(); break;
+            case 2: v2vService->followRequest(CAR_IP); break;
+            case 3: v2vService->followResponse(); break;
+            case 4: v2vService->stopFollow(CAR_IP); break;
+            case 5: v2vService->leaderStatus(50, 0, 100); break;
+            case 6: v2vService->followerStatus(50, 0, 10, 100); break;
+            default: return 0;
+        }
+    }
 }
--- v2v/v2v.cpp
+++ v2v/v2v.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "v2v.hpp"
 
 

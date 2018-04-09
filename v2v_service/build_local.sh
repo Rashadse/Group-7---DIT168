d1c4b426c47bb0ea44ffbeaf5700c3c44e06408c
maansthoernvik <thornvik.mans@gmail.com>
2018-04-09 11:47:04 +0200

    Added cmakelists for v2vservice and docker build script, cleaned up root

--- v2v_service/build_local.sh
+++ v2v_service/build_local.sh
@@ -4,4 +4,4 @@
 # VM and then transfering the built image to the car will not work.
 
 ./get_ip_address.py  # IMPORTANT! Gets the current IP address and passes to the container via a generated .txt
-docker build -t group7/carcode -f Dockerfile .
+docker build -t group7/v2vservice -f Dockerfile .

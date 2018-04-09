67702715045e1b244e34b66b8a2b4a4c7ff13f07
Rashad <rashadse@hotmail.com>
2018-04-09 20:33:42 +0200

    bug fixes, refactor

--- remoteControl/remoteControl.cpp
+++ remoteControl/remoteControl.cpp
@@ -29,43 +29,48 @@ int main(int /*argc*/, char** /*argv*/) {
     }
 	
 	opendlv::proxy::GroundSteeringReading msgSteering;
-    opendlv::proxy::PedalPositionReading msgPedal;
+   	opendlv::proxy::PedalPositionReading msgPedal;
 
 
-    std::cout << "To control the car use" << std::endl;
+    	std::cout << "To control the car use" << std::endl;
 	std::cout << "           W          " << std::endl;
 	std::cout << "    A             D   " << std::endl;
 	std::cout << "           S          " << std::endl;
 	std::cout << "Press X to emergency stop and quit" << std::endl;
 
 				
-				// A variable used to increment/decrement the percentage of the speed pedal
-                const int aLittleSpeed = 0.3;
+	// A variable used to increment/decrement the percentage of the speed pedal
+        //const int aLittleSpeed = 0.05;
                 
-				int pedalPercentage = 0;
+	float pedalPercentage;
 				
-				// A variable used as a condition for the while loop to know when to quit the remote control UI
-				bool loop = true;
+	// A variable used as a condition for the while loop to know when to quit the remote 		control UI
+	bool loop = true;
 
-                while (true) {
+                while (loop) {
 
-                string direction;
+			
+
+                char direction;
 
                 std::cin >> direction;
+
 
                 switch (direction) {
 
                     // accelerates
+
                     case 'w': 
                         
                         std::cout << "Accelerate" << std::endl;
                         msgSteering.steeringAngle(0.0);
                         od4.send(msgSteering);
                         //the car should accelerate as long as the speed is below 40%.
-                        if(msgPedal.percent <=40){
-                            PedalPercentage += aLittleSpeed;
+                	if (pedalPercentage <= 0.4){
+                            pedalPercentage += 0.1;
                         }
-                        msgPedal.percent(PedalPercentage);
+			std::cout << "Accelerate" << pedalPercentage << "." << std::endl;
+                        msgPedal.percent(pedalPercentage);
                         od4.send(msgPedal);
                          
                          break;
@@ -75,11 +80,12 @@ int main(int /*argc*/, char** /*argv*/) {
                         std::cout << "Decelerate" << std::endl;
                         msgSteering.steeringAngle(0.0);
                         od4.send(msgSteering);
-                        //the car should decelerate as long as the speed is above 5%. 
-                        if(msgPedal.percent >=5){
-                            PedalPercentage -= aLittleSpeed;
+                        //the car should decelerate as long as the speed is above 10%. 
+                        if (pedalPercentage >= 0.11){
+                            pedalPercentage -= 0.1;
                         }
-                        msgPedal.percent(PedalPercentage);
+                        
+                        msgPedal.percent(pedalPercentage);
                         od4.send(msgPedal);
 
                          break;
@@ -87,7 +93,7 @@ int main(int /*argc*/, char** /*argv*/) {
                      case 'a': 
                         std::cout << "Turning left" << std::endl;
                         //the car should slow down before turning if the speed is over 20%.
-                        if (PedalPercentage >= 0.2){
+                        if (pedalPercentage >= 0.2){
                         pedalPercentage = 0.2;}
                         od4.send(msgPedal);
                         msgSteering.steeringAngle(-15.0);
@@ -98,7 +104,7 @@ int main(int /*argc*/, char** /*argv*/) {
                      case 'd': 
                         std::cout << "Turning right" << std::endl;
                         //the car should slow down before turning if the speed is over 20%.
-                        if (PedalPercentage >= 0.2){
+                        if (pedalPercentage >= 0.2){
                         pedalPercentage = 0.2;}
                         od4.send(msgPedal);
                         msgSteering.steeringAngle(15.0);
@@ -108,20 +114,20 @@ int main(int /*argc*/, char** /*argv*/) {
                      
                      case 'x': 
                         std::cout << "Emergency Stopping" << std::endl;
-                        pedalPercentage = 0.0;
+                        pedalPercentage = 0;
                         od4.send(msgPedal);
                         msgSteering.steeringAngle(0.0);
                         od4.send(msgSteering);
-						std::cout << "quitting" << std::endl;
-						loop = false;
+			std::cout << "quitting" << std::endl;
+			loop = false;
                          break;
                      
                      default: 
-						cout << "wrong command, the car will now stop"<< endl;
-						pedalPercentage = 0.0;
-						od4.send(msgPedal);
-						msgSteering.steeringAngle(0.0);
-						od4.send(msgSteering);
+			std::cout << "wrong command, the car will now stop"<< std::endl;
+			pedalPercentage = 0;
+			od4.send(msgPedal);
+			msgSteering.steeringAngle(0.0);
+			od4.send(msgSteering);
                  }
 			}
 		}

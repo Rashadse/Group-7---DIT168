837f42eab74d963daf2490b518217efcdfebaea1
Rashad <rashadse@hotmail.com>
2018-04-12 01:10:45 +0200

    major refactoring and bug fixing

--- remotecontrol/remotecontrol.cpp
+++ remotecontrol/remotecontrol.cpp
@@ -1,74 +1,24 @@
-#include <cstdint>
-#include <chrono>
 #include <iostream>
-#include <sstream>
-#include <thread>
-
-#include "cluon/OD4Session.hpp"
-#include "cluon/Envelope.hpp"
-
-#include "messages.hpp"
+#include "remotecontrol.hpp"
 
 
-int main(int /*argc*/, char** /*argv*/) {
 
 
-std::shared_ptr<cluon::OD4Session> motorChannel = std::make_shared<cluon::OD4Session>(
-            180,
-            [](cluon::data::Envelope &&envelope) noexcept {
-                switch (envelope.dataType()) {
-                    case 4005: {
-                //        InternalGetAllGroupsResponse response = cluon::extractMessage<InternalGetAllGroupsResponse>(std::move(envelope));
-			//print response.groupid()
-//			response.groupid()
-//				internalChannel->send(response)
-                    }
-                    default: std::cout << "default case" << std::endl;
-                }
-            });
-
-//std::shared_ptr<cluon::OD4Session> internalChannel = std::make_shared<cluon::OD4Session>(
-//            181,
-//            [](cluon::data::Envelope &&envelope) noexcept {
-//                std::cout << "[OD4] ";
-//                switch (envelope.dataType()) {
-//                    case 4005: {
-                //        InternalGetAllGroupsResponse response = cluon::extractMessage<InternalGetAllGroupsResponse>(std::move(envelope));
-			//print response.groupid()
-//			response.groupid()
-//				internalChannel->send(response)
-//                   }
-//                    default: std::cout << "¯\\_(ツ)_/¯" << std::endl;
-//                }
-//            });
-
-	opendlv::proxy::GroundSteeringReading msgSteering;
-   	opendlv::proxy::PedalPositionReading msgPedal;
-	//InternalGetAllGroupsRequest message1;
-	
+int main() {
 		
-		
-	// A variable used to increment/decrement the percentage of the speed pedal
-	float pedalPercentage;
-				
-	// A variable used as a condition for the while loop to know when to quit the remote 		control UI
-	bool loop = true;
+	std::shared_ptr<remoteControl> remoteSending = std::make_shared<remoteControl>();
 
-                while (loop) {
+	unsigned char direction;
 
-                char direction;
-	
+    while (true) {
+
 	std::cout << "To control the car use" << std::endl;
 	std::cout << "           W          " << std::endl;
 	std::cout << "    A             D   " << std::endl;
 	std::cout << "           S          " << std::endl;
 	std::cout << "Press X to emergency stop and quit" << std::endl;
 
-
-		this_thread::sleep_for(chrono::milliseconds(2000));
-
-                std::cin >> direction;
-
+        std::cin >> direction;
 
                 switch (direction) {
 
@@ -76,6 +26,103 @@ std::shared_ptr<cluon::OD4Session> motorChannel = std::make_shared<cluon::OD4Ses
 		    
                     case 'w': {
                         
+			remoteSending->Accelerate();
+                         
+                         break;
+           		          }
+
+                    // decelerates
+                     case 's': {
+                         
+                        remoteSending->Decelerate();
+
+                         break;
+           		          }
+                    // Turns left
+                     case 'a': { 
+
+			remoteSending->Left();
+
+                         
+                         break;
+            		         }
+                    // Turns Right
+                     case 'd': { 
+
+			remoteSending->Right();
+		
+
+                         break;
+            		         }
+                    // Stops
+                     case 'x': {
+
+			remoteSending->Stop();	
+
+                         break;
+                		     }
+                     default: 
+			{
+			std::cout << "wrong command, try another command"<< std::endl;
+
+			break;
+			}                 
+		}
+	}
+}
+remoteControl::remoteControl() {
+	
+		std::shared_ptr<cluon::OD4Session> motorChannel = std::make_shared<cluon::OD4Session>(MOTOR_CHANNEL,
+        	[](cluon::data::Envelope &&envelope) noexcept {	
+
+/*
+
+                switch (envelope.dataType()) {
+                    case 4005: {
+                      InternalGetAllGroupsResponse response = cluon::extractMessage<InternalGetAllGroupsResponse>(std::move(envelope));
+			print response.groupid()
+			response.groupid()
+				internalChannel->send(response)
+                
+				  }
+                    default: { std::cout << "default case" << std::endl;
+                } }
+
+*/
+
+        	});
+
+		std::shared_ptr<cluon::OD4Session> internalChannel = std::make_shared<cluon::OD4Session>(INTERNAL_CHANNEL,
+        	[](cluon::data::Envelope &&envelope) noexcept {	
+
+/*                std::cout << "internal channel ";
+                switch (envelope.dataType()) {
+                    case 4005: {
+                	InternalGetAllGroupsResponse response = cluon::extractMessage<InternalGetAllGroupsResponse>(std::move(envelope));
+			print response.groupid()
+			response.groupid()
+				internalChannel->send(response)
+                   }
+                    default: {std::cout << "¯\\_(ツ)_/¯" << std::endl;
+                } } 
+
+*/ 	
+
+//InternalGetAllGroupsRequest message1;
+
+        	});
+
+	}
+
+	opendlv::proxy::GroundSteeringReading msgSteering;
+   	opendlv::proxy::PedalPositionReading msgPedal;
+
+	// A variable used to increment/decrement the percentage of the speed pedal
+	float pedalPercentage;
+				
+
+void remoteControl::Accelerate() {
+
                         std::cout << "Accelerate" << std::endl;
                         msgSteering.steeringAngle(0.0);
                         motorChannel->send(msgSteering);
@@ -86,12 +133,12 @@ std::shared_ptr<cluon::OD4Session> motorChannel = std::make_shared<cluon::OD4Ses
 			std::cout << "Accelerated by" << pedalPercentage << "." << std::endl;
                         msgPedal.percent(pedalPercentage);
                         motorChannel->send(msgPedal);
-                         
-                         break;
-           		          }
-                     case 's': {
-                         
-                        std::cout << "Decelerate" << std::endl;
+
+}
+
+void remoteControl::Decelerate() {
+
+			std::cout << "Decelerate" << std::endl;
                         msgSteering.steeringAngle(0.0);
                         motorChannel->send(msgSteering);
                         //the car should decelerate as long as the speed is above 10%. 
@@ -99,13 +146,14 @@ std::shared_ptr<cluon::OD4Session> motorChannel = std::make_shared<cluon::OD4Ses
                             pedalPercentage -= 0.1;
                         }
 
-                        std::cout << "Accelerated by" << pedalPercentage << "." << std::endl;
+                        std::cout << "Decelerated by" << pedalPercentage << "." << std::endl;
                         msgPedal.percent(pedalPercentage);
                         motorChannel->send(msgPedal);
 
-                         break;
-           		          }
-                     case 'a': { 
+}
+
+void remoteControl::Left() {
+
                         std::cout << "Turning left" << std::endl;
                         //the car should slow down before turning if the speed is over 20%.
                         if (pedalPercentage >= 0.2){
@@ -114,10 +162,11 @@ std::shared_ptr<cluon::OD4Session> motorChannel = std::make_shared<cluon::OD4Ses
 			std::cout << "turned left" << pedalPercentage << "." << std::endl;
                         msgSteering.steeringAngle(-15.0);
                         motorChannel->send(msgSteering);
-                         
-                         break;
-            		         }
-                     case 'd': { 
+
+}
+
+void remoteControl::Right() {
+
                         std::cout << "Turning right" << std::endl;
                         //the car should slow down before turning if the speed is over 20%.
                         if (pedalPercentage >= 0.2){
@@ -127,31 +176,15 @@ std::shared_ptr<cluon::OD4Session> motorChannel = std::make_shared<cluon::OD4Ses
                         msgSteering.steeringAngle(15.0);
                         motorChannel->send(msgSteering);
 
-                         break;
-            		         }
-                     case 'x': {
+}
+
+void remoteControl::Stop() {
+
                         std::cout << "Emergency Stopping" << std::endl;
                         pedalPercentage = 0;
 			msgPedal.percent(pedalPercentage);
                         motorChannel->send(msgPedal);
                         msgSteering.steeringAngle(0.0);
                         motorChannel->send(msgSteering);
-			std::cout << "quitting" << std::endl;
-			loop = false;
-                         break;
-                		     }
-                     default: 
-			{
-			std::cout << "wrong command, the car will now stop"<< std::endl;
-			pedalPercentage = 0;
-			msgPedal.percent(pedalPercentage);
-			motorChannel->send(msgPedal);
-			msgSteering.steeringAngle(0.0);
-			motorChannel->send(msgSteering);
-			}                 
-		}
-	}
-return 0;
+
 }
-
-
